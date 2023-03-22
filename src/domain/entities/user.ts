5746cd7153c0eeb5c93f52b356f6382bd858066b
pan <saran.swpy@gmail.com>
2023-03-22 16:35:03 +0700

    add sequelize models

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  id: number
+  name?: string
+  address?: string
+  email?: string
+  password?: string
+}
---
+++ src/infrastructure/sequelize/user.ts
@@ -0,0 +1,58 @@
+import * as Sequelize from 'sequelize';
+import { DataTypes, Model, Optional } from 'sequelize';
+import { IUser as UserAttributes } from '../../domain/entities';
+
+export type UserPk = "id";
+export type UserId = User[UserPk];
+export type UserOptionalAttributes = "id" | "name" | "email" | "password" | "address";
+export type UserCreationAttributes = Optional<UserAttributes, UserOptionalAttributes>;
+
+export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+  id!: number;
+  name?: string;
+  email?: string;
+  password?: string;
+  address?: string;
+
+
+  static initModel(sequelize: Sequelize.Sequelize): typeof User {
+    return User.init({
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    address: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'User',
+    timestamps: true,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+  }
+}
